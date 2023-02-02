e54375a2ddb5863ffb92bd1983386bd294b5f6f7
Diego Rodríguez Jacinto <93778802+diegorodrguez7@users.noreply.github.com>
2023-02-02 23:10:20 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+
+    safelist: [
+        'bg-red-500',
+        'bg-blue-500',
+        'bg-pink-500',
+        'bg-yellow-500',
+        'bg-green-500',
+        'bg-indigo-500',
+        'bg-purple-500',
+      ]
+};
